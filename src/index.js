d93c3d657b434bf4f572ae235947dd8d05cf2d73
RitaDyvnych <m.dyvnych@gmail.com>
2021-08-20 18:21:17 +0300

    начальная разметка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import { alert, error, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
+// error(
+//     {
+//         text: 'Work'
+//     }
+// )
